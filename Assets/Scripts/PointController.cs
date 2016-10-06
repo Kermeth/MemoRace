2c75cf42c33213da04cbc165e2f1d19aa986158c
CyberDev <denzelnihlus@gmail.com>
2016-10-06 03:28:09 +0200

    Point Controler
    Now you can make points appear on the screen

---
+++ Assets/Scripts/PointController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class PointController : MonoBehaviour {
+
+    private Animator anim
+    {
+        get
+        {
+            return this.GetComponent<Animator>();
+        }
+    }
+    private Text number
+    {
+        get
+        {
+            return this.GetComponentInChildren<Text>();
+        }
+    }
+
+    public void SetNumber(int number)
+    {
+        this.number.text = number.ToString();
+    }
+
+    private void Apear()
+    {
+        anim.SetBool("apear", true);
+    }
+
+    private void Disapear()
+    {
+        anim.SetBool("apear", false);
+    }
+
+    public void GetPressed()
+    {
+        //Check if good or bad press
+    }
+	
+}
---
+++ Assets/Scripts/PointPool.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PointPool : MonoBehaviour {
+
+    /// <summary>
+    /// Point Prefab
+    /// </summary>
+    public GameObject pointPrefab;
+
+    private RectTransform gamePanel
+    {
+        get
+        {
+            return this.GetComponent<RectTransform>();
+        }
+    }
+
+    void Start()
+    {
+        GeneratePoint();
+    }
+
+    public void GeneratePoint()
+    {
+        GameObject newPoint = Instantiate(pointPrefab);
+        newPoint.GetComponent<RectTransform>().SetParent(gamePanel);
+    }
+
+}
